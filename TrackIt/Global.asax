7ea673a346203577a2bc09063e4673eca1c1dee8
YOUR NAME <george.p.alexander@gmail.com>
2015-09-15 22:44:23 -0500

    Initiate commit

---
+++ TrackIt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrackIt.MvcApplication" Language="C#" %>
---
+++ TrackIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrackIt.Startup))]
+namespace TrackIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
